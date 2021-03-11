d102ceefb6d4a9e2b1ac4427c4d085119dc06c08
FedeD <62423005+Fede-debug@users.noreply.github.com>
2021-03-11 21:39:35 +0100

    Delete 5. REST service with Spring for tour of heroes/payroll directory
    just updating the right folders

--- 5. REST service with Spring for tour of heroes/payroll/src/main/java/com/example/payroll/HeroNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.payroll;
-
-class HeroNotFoundException extends RuntimeException {
-
-  HeroNotFoundException(Long id) {
-    super("Could not find employee " + id);
-  }
-}
--- 5. REST service with Spring for tour of heroes/payroll/src/main/java/com/example/payroll/HeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.payroll;
-
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface HeroRepository extends JpaRepository<Hero, Long> {
-
-}
